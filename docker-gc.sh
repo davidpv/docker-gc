6d9761b491344d0d42afff5f3ac892087ea5766a
David Pérez Vicens <davidpv@gmail.com>
2017-02-01 10:02:08 +0100

    titles

--- docker-gc.sh
+++ docker-gc.sh
@@ -17,11 +17,11 @@ echo $$ > $PID_DIR/dockergc
 
 
 $DOCKER ps -a
-echo "STOPING CONTAINERS"
+echo " === STOPING CONTAINERS === "
 $DOCKER stop $($DOCKER ps -a | grep -v docker-gc | awk 'FNR > 1 {print $1}') 2>/dev/null
-echo "DELETING CONTAINERS"
+echo " === DELETING CONTAINERS === "
 $DOCKER rm --force $($DOCKER ps -a | grep -v docker-gc | awk 'FNR > 1 {print $1}') >/dev/null
-echo "IMAGES TO DELETE"
+echo " === IMAGES TO DELETE === "
 $DOCKER images -a | tail -n+2  |sed 's/^\([^ ]*\) *\([^ ]*\) *\([^ ]*\).*/ \1:\2 \3 /' | grep -v -E "${EXCLUDE_IMAGES// /|}"
-echo "DELETING IMAGES"
+echo " === DELETING IMAGES === "
 $DOCKER rmi --force $($DOCKER images -a  | tail -n+2 | sed 's/^\([^ ]*\) *\([^ ]*\) *\([^ ]*\).*/ \1:\2 \3 /' | grep -v -E "${EXCLUDE_IMAGES// /|}"|  cut -d' ' -f3) 2>/dev/null
